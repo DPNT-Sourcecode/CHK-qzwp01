f047b4b8f2c050086472f393b5a779fcbb7c4a3d
sbx_user1051 <sbx_user1051@169.254.75.173>
2024-09-22 21:40:05 +0000

    Sun Sep 22 21:40:05 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,8 +6,7 @@ def checkout(skus):
     item_prices = {'A':50,'B':30,'C':20,'D':15, 'E':40}
     special_offers = {
         'A': [(5, 200), (3,130)],
-        'B': [(2,45)],
-        'E': [(2,0)]
+        'B': [(2,45)]
     }
     checkout_price = 0
     sku_counts = {}
@@ -17,29 +16,21 @@ def checkout(skus):
             return -1
         sku_counts[sku] = sku_counts.get(sku, 0) + 1
 
+    if 'E' in sku_counts:
+        free_B_count = sku_counts['E'] // 2
+        if 'B' in sku_counts:
+            sku_counts['B'] = max(0, sku_counts['B'] - free_B_count)
+        else:
+            sku_counts['B'] = 0
+
     for sku, count in sku_counts.items():
         if sku in special_offers:
-            for offer_count, offer_price in special_offers[sku]:
+            for offer_count, offer_price in sorted(special_offers[sku], reverse=True):
                 while count >= offer_count:
                     checkout_price += offer_price
                     count -= offer_count
         checkout_price += count * item_prices[sku]
 
 
-    if 'E' in sku_counts and 'B' in sku_counts:
-        count_E = sku_counts['E']
-        free_B_count = count_E // 2
-        sku_counts['B'] -= free_B_count
-        if sku_counts['B'] < 0:
-            sku_counts['B'] = 0
-
-    if 'B' in sku_counts:
-        count_B = sku_counts['B']
-        if count_B >= 2:
-            offer_count, offer_price = special_offers['B'][0]
-            while count_B >=2:
-                checkout_price +=offer_price
-                count_B -= offer_count
-        checkout_price += count_B * item_prices['B']
-
     return checkout_price
+
