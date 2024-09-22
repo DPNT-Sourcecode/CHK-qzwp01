7c4c0dc5d0ee98cee7fb3dc2baa7f428a4c68102
sbx_user1051 <sbx_user1051@169.254.75.173>
2024-09-22 17:23:12 +0000

    Sun Sep 22 17:23:12 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -25,16 +25,22 @@ def checkout(skus):
                     count -= offer_count
         checkout_price += count * item_prices[sku]
 
-        if 'E' in sku_counts and 'B' in sku_counts:
-            count_E = sku_counts['E']
-            if count_E >= 2:
-                free_B_count = count_E // 2
-                sku_counts['B'] -= free_B_count
-                if sku_counts['B'] < 0:
-                    sku_counts['B'] = 0
+
+    if 'E' in sku_counts and 'B' in sku_counts:
+        count_E = sku_counts['E']
+        free_B_count = count_E // 2
+        sku_counts['B'] -= free_B_count
+        if sku_counts['B'] < 0:
+            sku_counts['B'] = 0
 
     if 'B' in sku_counts:
         count_B = sku_counts['B']
+        if count_B >= 2:
+            offer_count, offer_price = special_offers['B'][0]
+            while count_B >=2:
+                checkout_price +=offer_price
+                count_B -= offer_count
         checkout_price += count_B * item_prices['B']
 
     return checkout_price
+
