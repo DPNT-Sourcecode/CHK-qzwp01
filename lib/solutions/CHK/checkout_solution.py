b6216bfa5703f42e2680432616a6807ca38c1fd3
sbx_user1051 <sbx_user1051@169.254.75.173>
2024-09-22 22:26:26 +0000

    Sun Sep 22 22:26:26 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,28 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    item_prices = {'A':50,'B':30,'C':20,'D':15, 'E':40, 'F':10}
+    item_prices = {'A':50,'B':30,'C':20,'D':15, 'E':40, 'F':10,
+                   'G': 20, 'H': 10, 'I':35, 'J':60, 'K':80,
+                   'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 'Q':30,
+                   'R':50, 'S':30, 'T':20, 'U':40, 'V':50, 'W':20,
+                   'X':90, 'Y':10, 'Z':50
+                   }
     special_offers = {
         'A': [(5, 200), (3,130)],
         'B': [(2,45)],
-        'F': [(3, 20)]
+        'F': [(3, 20)],
+        'H': [(10, 80), (5, 45)],
+        'K': [(2,150)],
+        'P':[(5, 200)],
+        'Q':[(3, 80)],
+        'V':[(3,130), (2, 90)]
+    }
+
+    free_offers = {
+        'E': ('B', 2),
+        'N': ('M', 3),
+        'R': ('Q', 3),
+        'U': ('U', 3)
     }
     checkout_price = 0
     sku_counts = {}
@@ -17,12 +34,11 @@ def checkout(skus):
             return -1
         sku_counts[sku] = sku_counts.get(sku, 0) + 1
 
-    if 'E' in sku_counts:
-        free_B_count = sku_counts['E'] // 2
-        if 'B' in sku_counts:
-            sku_counts['B'] = max(0, sku_counts['B'] - free_B_count)
-        else:
-            sku_counts['B'] = 0
+    for offer_sku, (free_sku, qty_required) in free_offers.items():
+        if offer_sku in sku_counts:
+            free_count = sku_counts[offer_sku] // qty_required
+            if free_sku in sku_counts:
+                sku_counts[free_sku] = max(0, sku_counts[free_sku] - free_count)
 
     for sku, count in sku_counts.items():
         if sku in special_offers:
@@ -34,3 +50,4 @@ def checkout(skus):
 
 
     return checkout_price
+
