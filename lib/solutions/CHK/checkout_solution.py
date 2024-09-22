98323a781582f0dc07c6c1fa098f8b8d7467b1ae
sbx_user1051 <sbx_user1051@169.254.75.173>
2024-09-22 16:33:53 +0000

    Sun Sep 22 16:33:53 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,16 +3,35 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    item_prices = {'A':50,'B':30,'C':20,'D':15}
+    item_prices = {'A':50,'B':30,'C':120,'D':115, 'E':40}
     checkout_price = 0
+    sku_counts = {}
 
     for sku in skus:
         if sku not in item_prices:
             return -1
-        checkout_price += item_prices[sku]
+        sku_counts[sku] = sku_counts.get(sku, 0) + 1
 
-    if skus.count('A') >= 3:
-        checkout_price -= (skus.count('A') // 3) * (3 * item_prices['A'] - 130)
-    if skus.count('B') >= 2:
-        checkout_price -= (skus.count('B') // 2) * (2 * item_prices['B'] - 45)
+    for sku, count in sku_counts.items():
+        checkout_price += count * item_prices[sku]
+
+    if 'A' in sku_counts:
+        count_A = sku_counts['A']
+        checkout_price -= (count_A//5) * (5 * item_prices['A'] - 200)
+        checkout_price -= (count_A//3) * (3 * item_prices['A'] - 130)
+
+    if 'B' in sku_counts:
+        count_B = sku_counts['B']
+        checkout_price-= (count_B //2) * (2 * item_prices['B'] - 45)
+
+    if 'E' in sku_counts:
+        count_E = sku_counts['E']
+        if count_E >= 2 and 'B' in sku_counts:
+            count_B_free = count_E // 2
+            if sku_counts['B'] < 0:
+                sku_counts['B'] = 0
+
+    if 'B' in sku_counts:
+        checkout_price += sku_counts['B'] * item_prices['B']
     return checkout_price
+
