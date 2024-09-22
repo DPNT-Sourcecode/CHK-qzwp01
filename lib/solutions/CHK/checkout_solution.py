169ffa02e7d33f011d0ca480188ef8e081d11c13
sbx_user1051 <sbx_user1051@169.254.75.173>
2024-09-22 23:23:52 +0000

    Sun Sep 22 23:23:52 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,7 @@
 # skus = unicode string
 def checkout(skus):
     item_prices = {'A':50,'B':30,'C':20,'D':15, 'E':40, 'F':10,
-                   'G': 20, 'H': 10, 'I':35, 'J':60, 'K':80,
+                   'G': 20, 'H': 10, 'I':35, 'J':60, 'K':70,
                    'L':90, 'M':15, 'N':40, 'O':10, 'P':50, 'Q':30,
                    'R':50, 'S':20, 'T':20, 'U':40, 'V':50, 'W':20,
                    'X':17, 'Y':20, 'Z':21
@@ -75,4 +75,5 @@ def checkout(skus):
 
 
     return checkout_price
+
 
