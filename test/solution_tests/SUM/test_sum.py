8c9f9a67c7d2bb4f013a33590381b663d490e55b
sbx_user1051 <sbx_user1051@169.254.42.193>
2024-09-22 14:44:58 +0000

    Sun Sep 22 14:44:58 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,8 +1,12 @@
+import sys
 from solutions.SUM import sum_solution
+
+sys.path.append("/Users/emmanuel/Desktop/accelerate_runner")
 
 # will switch to pycharm will swictch to personal laptop
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
 
