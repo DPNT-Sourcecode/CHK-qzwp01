fcef89573ce54a7e3acf75b9f3f2b36f6294bf26
sbx_user1051 <sbx_user1051@169.254.42.193>
2024-09-22 14:20:58 +0000

    Sun Sep 22 14:20:58 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,7 +1,8 @@
 from solutions.SUM import sum_solution
 
-# will switch to pycharm
+# will switch to pycharm will swictch to personal laptop
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
