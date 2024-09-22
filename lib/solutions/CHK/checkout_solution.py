46de355bd324a718f9b3db4bd269a6917e00947a
sbx_user1051 <sbx_user1051@169.254.75.173>
2024-09-22 16:14:46 +0000

    Sun Sep 22 16:14:46 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,17 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    item_prices = {'A':50,'B':30,'C':20,'D':15}
+    checkout_price = 0
+
+    for sku in skus:
+        if sku not in item_prices:
+            return False
+        checkout_price += item_prices[sku]
+
+    if skus.count('A') >= 3:
+        checkout_price -= (skus.count('A') // 3) * (3 * item_prices['A'] - 130)
+    if skus.count('B') >= 2:
+        checkout_price -= (skus.count('B') // 2) * (2 * item_prices['B'] - 45)
+    return checkout_price
+
